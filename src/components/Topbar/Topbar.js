a7aba15cfcad0960fc6c2194e0ea9e70f7894754
godson imiebo <imiebo.godson@gmail.com>
2022-09-25 21:57:49 +0100

    minor changes

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -4,7 +4,7 @@ const Topbar = () => {
   return (
     <div className='topbar'>
       <div className='topbar-left-items'>
-        <p className='topbar-large-text'>Welcome, Josiah &#128075;</p>
+        <p className='topbar-large-text'>Welcome, Chief &#128075;</p>
         <p className='topbar-smaller-text'>Another day to do something special, right?.</p>
       </div>
       <div className='topbar-right-items'></div>
