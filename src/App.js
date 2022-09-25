b4db62d5489ae18bc85fe0260b2dfad140134ab6
godson imiebo <imiebo.godson@gmail.com>
2022-09-25 17:46:39 +0100

    added Form component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import Form from './components/Form/Form';
 import TableDisplay from './components/TableDisplay/TableDisplay';
 import Topbar from './components/Topbar/Topbar';
 
 function App() {
   return (
     <div className="app">
-      <section className='left-section'></section>
+      <section className='left-section'>
+        <Form />
+      </section>
       <section className='right-section'>
         <Topbar />
         <main className='main-section'>
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,8 +1,55 @@
+import { useState } from 'react'
 import './Form.css'
 
 const Form = () => {
+
+  const [name, setName] = useState()
+    const [description, setDescription] = useState()
+
+
+    const onNameChange = (e) => {
+        setName(e.target.value)
+    }
+
+    const onDescriptionChange = (e) => {
+        setDescription(e.target.value)
+    }
+
+
+    const onFormSubmit = (e) => {
+        e.preventDefault()
+        const userDetails = { name, description }
+        console.log(userDetails)
+        // addUser(userDetails)
+
+        setName('')
+        setDescription('')
+
+        // if (user) {
+        //     setName('')
+        //     setEmail('')
+        //     setPassword('')
+            
+        //     setTimeout(() => {
+        //         refetchUsers()
+        //     }, 5000);
+        // }
+
+    }
+
   return (
-    <div>Form</div>
+    <form onSubmit={(e) => onFormSubmit(e)}>
+        <p className='form-title'>ADD PRODUCT</p>
+        <div className='form-wrapper'>
+            <div className='form-item'>
+                <input onChange={onNameChange} className="" type="text" name="name" placeholder='Name' value={name}/>
+            </div>
+            <div className="form-item">
+                <textarea onChange={onDescriptionChange} name="description" placeholder='Product Description' value={description}></textarea>
+            </div>
+        </div>
+        <button type='submit'>Submit</button>
+    </form>
   )
 }
 
